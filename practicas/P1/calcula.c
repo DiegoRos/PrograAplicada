03fd191fc5097c93b8ba57097748db14278ce142
DiegoRos <diegorosenberg@gmail.com>
2021-01-31 23:15:24 -0600

    31/01/2021

--- practicas/P1/calcula.c
+++ practicas/P1/calcula.c
@@ -7,13 +7,16 @@
 */
 #include "misDefiniciones.h"
 
-extern int parseEntradas(char *arg1, char *arg2, int *operador1, int *operador2);
+extern int encontrarOperacion(int argc, char *argv[], char *operacion, int *error_operacion);
+extern float hacerOperacion(int *operador1, int *opeardor2, char operacion, int *error_val);
+extern int parseEntradas(char *arg1, char *arg2, int *operador1, int *operador2, int *error_val);
 extern int esBinario(char *arg);
-extern int esHexadecimal(char *arg);
 extern int binarioADecimal(char *arg);
 extern int decimalAOctal(int valor_decimal);
 
 int main(int argc, char *argv[]){
+	int *error_val = (int *)malloc(sizeof(int));
+
 	if(argc != 4){
 		printf("Número incorrecto de entradas, se esperan 3 (sin incluir el ejecutable) y se encontraron %d\n", argc);
 	}
@@ -22,63 +25,38 @@ int main(int argc, char *argv[]){
 		float resultado;
 		int i, print = True;
 		int *operador1 = (int *)malloc(sizeof(int)), *operador2 = (int *)malloc(sizeof(int));
-		char operacion;
-
-		for(i = 1; i < argc; i++){
-			if((*argv[i] == 'x') || (*argv[i] == '/') || (*argv[i] == '+') || (*argv[i] == '-')){
-				operacion = argv[i][0];
-				break;
-			}
-		}
-	
-		if(i == 1){
-			parseEntradas(argv[2], argv[3], operador1, operador2);
-		}
-
-		else if(i == 2){
-			parseEntradas(argv[1], argv[3], operador1, operador2);
-		}
-
-		else{
-			parseEntradas(argv[1], argv[2], operador1, operador2);
-		}
+		char *operacion = (char *)malloc(sizeof(char));
+		*error_val = 0;
 		
-		//Revisa si ambos operadores tienen espacio alocado
-		if(operador1 && operador2){
+		i = encontrarOperacion(argc, argv, operacion, error_val);
+		if (*error_val == 0){
+			if (i == 1){
+				parseEntradas(argv[2], argv[3], operador1, operador2, error_val);
+			}
+
+			else if (i == 2){
+				parseEntradas(argv[1], argv[3], operador1, operador2, error_val);
+			}
+
+			else{
+				parseEntradas(argv[1], argv[2], operador1, operador2, error_val);
+			}
 		
-			switch(operacion){
-				case '+':	
-					resultado = *operador1 + *operador2;
-					break;
+		
+			resultado = hacerOperacion(operador1, operador2, *operacion, error_val);
+		}
 
-				case '-':
-					resultado = *operador1 - *operador2;
-					break;
-
-				case 'x':
-					resultado = (*operador1) * (*operador2);
-					break;
-
-				case '/':
-					resultado = (float)(*operador1) / (float)(*operador2);
-					break;
-
-				default:
-					printf("No se metió un operador válido\n");
-					print = False;
-					break;
-			}
-
-			free(operador1);
-			free(operador2);
-			
-
-			if(print){
-				printf("Resultado en Decimal: %f\n", resultado);
-				printf("Resultado en Octal: %d\n", decimalAOctal((int) resultado));
-			}
+		free(operador1);
+		free(operador2);
+		free(operacion);
+		
+		if(*error_val == 0){
+			printf("Resultado en Decimal: %f\n", resultado);
+			printf("Resultado en Octal: %d\n", decimalAOctal((int) resultado));
 		}
 	}
+	
+	free(error_val);
 	return 0;
 }
 
--- practicas/P1/funciones.c
+++ practicas/P1/funciones.c
@@ -12,15 +12,46 @@
 /*
 *	@brief: Función que revisa si string entrada es un número binario, regresa un booleano
 *	@author: Equipo 3
+*	@param int argc: Número de entradas
+*	@param char *argv[]: Entradas de la línea de comando
+*	@param char *operacion: Puntero a la operación entrada por el usuario
+*	@param int *error_val: Valor de error para el algoritmo
+*	@return int
+*/
+int encontrarOperacion(int argc, char *argv[], char *operacion, int *error_val){
+	int i;
+	int condicion_longitud, condicion_operador;
+	for(i = 1; i < argc; i++){
+		condicion_longitud = strlen(argv[i]) == 1;
+		condicion_operador = (*argv[i] == 'x') || (*argv[i] == '/') || (*argv[i] == '+') || (*argv[i] == '-');	
+
+		if(condicion_longitud && condicion_operador){
+			*operacion = argv[i][0];
+			break;
+		}
+	}
+	
+	if (*operacion == '\0'){
+		printf("No se metión un operador válido\n");
+		*error_val = 1;
+	}
+
+	return i;
+}
+
+/*
+*	@brief: Función que revisa si string entrada es un número binario, regresa un booleano
+*	@author: Equipo 3
 *	@param char *arg: Primera entrada que no sea un operador
 *	@return int
 */
-
 int esBinario(char *arg){
-	int i;
+	int i = 0;
+	if (arg[i] == '-') 
+		i++;
 
-	if ((arg[0] == '0') && (arg[1] == 'b')){
-		for(i = 2; i < strlen(arg); i++){
+	if ((arg[i] == '0') && (arg[i + 1] == 'b')){
+		for(i = i + 2; i < strlen(arg); i++){
 			if(!((arg[i] == '0') || (arg[i] == '1'))){
 				return False;
 			}
@@ -28,7 +59,7 @@ int esBinario(char *arg){
 	} 
 	
 	else{
-		for(i = 0; i < strlen(arg); i++){
+		for(i = i; i < strlen(arg); i++){
 			if(!((arg[i] == '0') || (arg[i] == '1'))){
 				return False;
 			}		
@@ -41,21 +72,24 @@ int esBinario(char *arg){
 /*
 *	@brief: Función que revisa si string entrada es un número hexadecimal, regresa un booleano
 *	@author: Equipo 3
-*	@param char *arg: String a revisar
+*	@param char *arg: Primera entrada que no sea un operador
 *	@return int
 */
 int esHexadecimal(char *arg){
-	int i;
-	if ((arg[0] == '0') && (arg[1] == 'x')){
-		for(i = 2; i < strlen(arg); i++){
+	int i = 0;
+	if (arg[i] == '-') 
+		i++;
+
+	if ((arg[i] == '0') && (arg[i + 1] == 'x')){
+		for(i = i + 2; i < strlen(arg); i++){
 			if(!isxdigit(arg[i])){
 				return False;
-			}		
-		}
-	}
+			}
+		}	
+	} 
 	
 	else{
-		for(i = 0; i < strlen(arg); i++){
+		for(i = i; i < strlen(arg); i++){
 			if(!isxdigit(arg[i])){
 				return False;
 			}		
@@ -72,15 +106,21 @@ int esHexadecimal(char *arg){
 *	@return int
 */
 int binarioADecimal(char *arg){
-	int cantidad = strlen(arg), i;
+	int cantidad = strlen(arg), i = 0;
 	int resultado = 0;
-	for(i = 0; i < cantidad; i++){
+
+	if (arg[i] == '-') i++;
+
+	for(i = i; i < cantidad; i++){
 		if(arg[i] == '1'){
 			resultado += pow(2, cantidad - 1 - i);
 		}
 	}
+	if (arg[0] == '-') resultado *= -1;
+
 	return resultado;
 }
+
 /*
 *	@brief: Función que asigna el valor correcto hexadecimal o binario al valor de operador1 y operador2
 *	@author: Equipo 3
@@ -88,27 +128,60 @@ int binarioADecimal(char *arg){
 *	@param char *arg2: Segunda entrada que no sea un operador, es un número hexadecimal
 *	@param int *operador1: Número binario en forma decimal
 * 	@param int *operador2: Número hexadecimal en forma decimal
+*	@param int *error_val: Valor de error para el algoritmo
 *	@return int
 */
-
-int parseEntradas(char *arg1, char *arg2, int *operador1, int *operador2){
-	if ((esBinario(arg1)) && (esHexadecimal(arg2))){
+int parseEntradas(char *arg1, char *arg2, int *operador1, int *operador2, int *error_val){
+	if (esBinario(arg1) && esHexadecimal(arg2)){
 		*operador1 = binarioADecimal(arg1);
 		sscanf(arg2, "%x", operador2);
 	}
 
-	else if ((esBinario(arg2)) && (esHexadecimal(arg1))){
+	else if (esBinario(arg2) && esHexadecimal(arg1)){
 		*operador2 = binarioADecimal(arg2);
 		sscanf(arg1, "%x", operador1);
 	}
 	
 	else{
 		printf("Las entradas numéricas no son correctas\n");
+		*error_val = 1;
 	}
 	
 	return 0;
 }
 
+/*
+*	@brief: Que hace la operación pedida por el usuario
+*	@author: Equipo 3
+*	@param int *operador1: Primer valor numérico
+*	@param int *operador2: Segundo valor numérico
+*	@param char operacion: Operación a hacer
+*	@param int *error_val: Valor de error para el algoritmo
+*	@return int
+*/
+float hacerOperacion(int *operador1, int *operador2, char operacion, int *error_val){
+	float resultado;
+	//Revisa si ambos operadores tienen espacio alocado
+	switch(operacion){
+		case '+':	
+			resultado = *operador1 + *operador2;
+			break;
+
+		case '-':
+			resultado = *operador1 - *operador2;
+			break;
+
+		case 'x':
+			resultado = (*operador1) * (*operador2);
+			break;
+
+		case '/':
+			resultado = (float)(*operador1) / (float)(*operador2);
+			break;
+		}
+
+	return resultado;
+}
 
 /*
 *	@brief: Función que toma un número decimal y lo cambia a octal
@@ -118,12 +191,20 @@ int parseEntradas(char *arg1, char *arg2, int *operador1, int *operador2){
 */
 int decimalAOctal(int valor_decimal){
 	int numero_octal = 0, i = 1;
+	int es_negativo = 1;
+
+	if (valor_decimal < 0){
+		es_negativo = -1;
+		valor_decimal *= -1;
+	}
 
 	while (valor_decimal > 0){
 		numero_octal += (valor_decimal % 8) * i;
 		valor_decimal = (int) (valor_decimal / 8);
 		i *= 10;
 	}
+
+	numero_octal *= es_negativo;
 
 	return numero_octal;	
 }
