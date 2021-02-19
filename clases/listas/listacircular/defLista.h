75c0b066a43b48894cba330cec275c68393ee05d
DiegoRos <diegorosenberg@gmail.com>
2021-02-19 15:13:25 -0600

    19/02/2021
    19/02/2021

---
+++ clases/listas/listacircular/defLista.h
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Datos{
+	char nombre[40];
+	int prioridad;
+}Info;
+
+typedef struct ListaCircular{
+	Info solicitud;
+	struct ListaCircular *next;
+}ListaCircular;
+
+ListaCircular * anexarNodo(Info dato, ListaCircular *pt){
+	ListaCircular *aux, *nodo_nuevo;
+	aux = pt;
+
+	nodo_nuevo = (ListaCircular *)malloc(sizeof(ListaCircular));
+	nodo_nuevo->solicitud = dato;
+
+	if (pt == NULL){
+		nodo_nuevo->next = nodo_nuevo;
+		return nodo_nuevo;
+	}	
+
+	while (aux->next != pt){
+		aux = aux->next;
+	}
+	aux->next = nodo_nuevo;
+	nodo_nuevo->next = pt;
+	
+	return pt;	
+}
+
+ListaCircular * borrarNodo(ListaCircular *pt){
+	ListaCircular *nodo_inicial = pt->next;
+	ListaCircular *aux = pt;
+	ListaCircular *nodo_final = pt;
+
+	if (pt == NULL){
+		printf("Lista vac�a\n");
+	}
+	else if(pt == pt->next){
+		pt = NULL;
+		free(aux);
+	}
+	else{
+		while(nodo_final->next != pt){
+			nodo_final = nodo_final->next;
+		}
+		nodo_final->next = nodo_inicial;
+		pt = nodo_inicial;
+		free(aux);
+	}
+	return pt;
+}
+
+ListaCircular * borrarNodoPrioridad(int prioridad, ListaCircular *pt){
+	ListaCircular *aux = pt, *nodo_inicial = pt;
+	if (pt == NULL){
+		printf("Lista Vacía\n");
+	}
+	else{
+		do{
+			if (pt->solicitud.prioridad == prioridad){
+				if(pt == nodo_inicial){
+					nodo_inicial = pt->next;
+				}
+				pt = borrarNodo(pt);
+			}
+			else{
+				pt = pt->next;
+			}
+			printf("\tNombre: %s \tP:%i\n", pt->solicitud.nombre, pt->solicitud.prioridad);
+			printf("\t\tSiguiente: %s\tInicial: %s\n",pt->next->solicitud.nombre,nodo_inicial->solicitud.nombre);
+		}while(pt->next != nodo_inicial);
+	}
+
+	if(pt->solicitud.prioridad == prioridad){
+		pt = borrarNodo(pt);
+	}
+	else{
+		pt = pt->next;
+	}
+	printf("\n");
+	return pt;
+}
+
+/*
+*	@brief: Esta función imprime un stack entero
+*	@author: Diego Rosenberg de Angoitia
+*	@param ListaCircular *pt
+*	@return int
+*/
+int imprimirCola(ListaCircular *pt){
+	ListaCircular *aux = pt;
+
+	if (pt == NULL){
+		printf("La lista está vacía.\n");
+	}
+
+	else{
+		do{
+			printf("\tNombre: %s \tPrioridad: %i\n", aux->solicitud.nombre, aux->solicitud.prioridad);
+			aux = aux->next;
+		}while(aux != pt);
+	}
+	return 1;
+}
+
---
+++ clases/listas/listacircular/listSimpleCirc.c
@@ -0,0 +1,58 @@
+/*
+*	@file: listSimpleCirc.c	
+*	@brief: Se hará un programa que construya una lista simple circular ocn reglas:
+*		Reglas o procesos para crear una COla de antención de solicitudes.
+			1.- Una cola vacía en un inicio es NULL
+			2.- Cuando tenga un solo nodo este se apunta a si mismo.
+			3.- Cuand llega  un segundo y subsecuente nodo, debo ir al final de la lista e insertarlos ahí. 
+				¿Cómo sé que es el final de la lista? Por que un puntero auxiliar recorre tod y  llega a auxiliar-> next = inicial
+			4.- Para borrar un nodo atendido debo borrar el primero de la lista.
+			5.- Si establezco una prioridad par borarr nodos del queue.
+			6.- Nunca debo perder el inicio del queue. 
+*	@author: Clase Guillermo Gómez Abascal
+*	@date: 19/02/2021
+*/
+
+#include "defLista.h"
+
+//nodo * anexaNodo(Info *dat, ListaCircular *pt);
+//int imprimirCola(ListaCircular *pt);
+//nodo * borrarNodo(ListaCircular *pt);
+
+int main(int argc, char *argv[]){
+	Info datos;
+	ListaCircular *inicio;
+	FILE *fp;
+	
+	inicio = NULL;
+	fp = fopen(argv[1], "r");
+	
+	if (fp == NULL){
+		printf("\nArchivo no disponible\n");
+		exit(1);
+	}
+	while(fscanf(fp, "%[^\t]%i\n", datos.nombre, &datos.prioridad) == 2){
+		inicio = anexarNodo(datos, inicio);
+	}
+
+	fclose(fp);
+
+	printf("\nImpresión de la cola creada:\n");
+	imprimirCola(inicio);
+
+	printf("\nBorrar Prioridad 1.\n");
+	int prioridad = 1;
+	inicio = borrarNodoPrioridad(prioridad, inicio);
+	imprimirCola(inicio);
+	
+	/*
+	printf("\nAtender todas las solicitudes:\n");
+	while (inicio != NULL){
+		printf("\nNodo a borrar: %s\t%i\n", inicio->solicitud.nombre, inicio->solicitud.prioridad);
+		inicio = borrarNodo(inicio);
+		printf("\nLista Resultante:\n");
+		imprimirCola(inicio);
+	}
+	*/
+	return 0;
+}
