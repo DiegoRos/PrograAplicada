8242ae458b1b9a89768ceb12ebb45a076dc96705
DiegoRos <diegorosenberg@gmail.com>
2021-03-16 11:00:50 -0600

    Added first steps of Práctica 3, including required structs, main.c logic and some empty functions in functions.c

---
+++ clases/arbolesBinarios/defArbol.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Arbol{
+	int val;
+	struct Arbol *left, *right;
+}Arbol;
+
+Arbol * insertar(Arbol *pt, int num){
+
+}
+
+void recorrer(Arbol *pt){
+
+}
+
+int buscarNumero(Arbol *pt, int num){
+
+
+}
+
+void encontrarPares(Arbol *pt){
+
+}
+
+int buscarDiercto(Arbol *pt, int num){
+
+}
+
+int recorrerDescendente(Arbol *pt){
+
+}
+
---
+++ clases/arbolesBinarios/main.c
@@ -0,0 +1,6 @@
+#include "defArbol.h"
+
+int main(int argc, char *argv[]){
+
+
+}
