2b5f773e1a764de8768289cc36a480eb6c2a7262
DiegoRos <diegorosenberg@gmail.com>
2021-04-21 12:56:28 -0500

    Casi terminado la máquina de chiclces

--- clases/maquina/maquina limpia/chicles.c
+++ clases/maquina/maquina limpia/chicles.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "tables.h"
 
 
@@ -109,7 +110,7 @@ void getevent(void)
 /* FUNCIONES DE IMPLEMENTACION */
 int preguntarColor(void){
 	int i;
-	//system("clear");
+	system("clear");
 	for(i = 0; i < 2; ++i){
 		if(strcmp(event.args, clientes_totales[i].num_cliente) == 0){
 			cliente_temp = clientes_totales[i];
@@ -126,32 +127,75 @@ int preguntarColor(void){
 int indicarErrorMoneda(void){
 	system("clear");
 	printf("\nSe ingresó un monedero inválido, intente de nuevo.\n");
+	
+	sleep(2);
+	
+	system("clear");
+	printf("\nQuieres un chicle? Cuesta $1.00, Ingresa tu tarjeta: [M:****]\n");
 	return 0;
 }
 
 int preImprimirSolicitud(void){
-
+	int color;
+	sscanf(event.args, " %i", &color);
+	system("clear");
+	printf("\nUsted ha elejido el chicle de color: %s\n", (color == 1) ? "Amarillo" : ((color == 2) ? "Verde" : (color == 3 ? "Rojo": "Color no válido")));
+	
+	sleep(2);
 	return 0;
 }
 
 int indicarErrorColor(void){
 	system("clear");
 	printf("\nEscoge un color válido\n");
+	sleep(2);
+
+	system("clear");
+	printf("\nChicles: 1 = Amarillo, 2 = Verde, 3 = Rojo\n");
+	printf("Hola %s, escoge un color de Chicle C:[1|2|3]", cliente_temp.nombre);	
 	return 0;
 }
 
 int revisarSaldo(void){
-	
-	return 0;
+	int flag_saldo = 0;
+	if(cliente_temp.saldo >= 1){//Alcanza
+		flag_saldo = 1; // Posición en arreglo de aux_table
+	}
+	else{
+		flag_saldo = 2; // Posición en arreglo de aux_table
+	}
+	return flag_saldo;
 }
 
 int entregarChicleYActualizarSaldo(void){
+	FILE *fp;
+	int i;
+	cliente_temp.saldo -= 1;
+	
+	system("clear");
+	printf("\nToma tu chicle y tu nuevo saldo es: %f\n", cliente_temp.saldo);
+	fp = fopen("clientes.txt", "w");
+	//Obviamente esto sería mejor con una lista o alguna otra estructura
+	for(i = 0; i < 2; ++i){
+		fprintf(fp, "%s\t%s\t%f\n",clientes_totales[i].nombre, clientes_totales[i].num_cliente, clientes_totales[i].saldo);
+	}
 
+	fclose(fp);
+	
+	sleep(2);
+	system("clear");
+	printf("\nQuieres un chicle? Cuesta $1.00, Ingresa tu tarjeta: [M:****]\n");
 	return 0;
 }
 
 int indicarErrorSaldo(void){
+	char esperar;
+	system("clear");
+	printf("\nNo hay suficiente saldo en el monedero, intentar de nuevo.\n");
 
+	sleep(2);
+	system("clear");
+	printf("\nQuieres un chicle? Cuesta $1.00, Ingresa tu tarjeta: [M:****]\n");
 	return 0;
 }
 
